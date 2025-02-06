5443bcd2a3d80b687d3ca3fcb736f7d3e7691154
Shubham Prakash <prakashshubham26@gmail.com>
2025-02-06 22:42:19 +0530

    Improved Code readability

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,6 @@ app.use("/api/auth", authRoutes);
 app.use("/api/budgets", budgetRoutes);
 app.use("/api/expenses", expenseRoutes);
 
-// 404 handler
 app.use((req, res, next) => {
   res.status(404).json({ message: `Cannot ${req.method} ${req.url}` });
 });
